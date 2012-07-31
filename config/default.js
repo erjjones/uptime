99152bd23bfe1535713a3c6ffabadc2f173e9979
Adam Alexander <aalexander@exacttarget.com>
2012-07-31 07:33:47 -0400

    Some configuration to improve performance

--- config/default.js
+++ config/default.js
@@ -13,13 +13,13 @@ module.exports = {
 		name: 'origin',
 		apiUrl: 'http://localhost:' + process.env.PORT + '/api',
 		pollingInterval: 10000, // ten seconds
-		timeout: 5000 // five seconds
+		timeout: 9000 // nine seconds
 	},
 
 	analyzer: {
 		updateInterval: 60000, // one minute
 		qosAggregationInterval: 600000, // ten minutes
-		pingHistory: 8035200000 // three months
+		pingHistory: 1000*60*60*24 // twenty-four hours
 	},
 
 	autoStartMonitor: true,
@@ -28,6 +28,6 @@ module.exports = {
 		port: process.env.PORT
 	},
 
-	verbose: true // only used in dev
+	verbose: false // only used in dev
 
 };
